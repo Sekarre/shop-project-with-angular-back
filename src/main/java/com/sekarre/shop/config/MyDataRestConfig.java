5481547bdaff7e0ce5ce9301e56a82c99b803c48
Sekarre <zimny2121@gmail.com>
2021-06-06 17:24:47 +0200

    Exposed ids, added method in product repo

--- src/main/java/com/sekarre/shop/config/MyDataRestConfig.java
+++ src/main/java/com/sekarre/shop/config/MyDataRestConfig.java
@@ -2,22 +2,38 @@ package com.sekarre.shop.config;
 
 import com.sekarre.shop.entity.Product;
 import com.sekarre.shop.entity.ProductCategory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+@RequiredArgsConstructor
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE};
 
-        //disabling methods for product
         setUnsupportedActions(config, theUnsupportedActions, Product.class);
         setUnsupportedActions(config, theUnsupportedActions, ProductCategory.class);
+
+        exposeIds(config);
+    }
+
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities()
+                .stream()
+                .map(Type::getJavaType)
+                .toArray(Class[]::new));
     }
 
 
